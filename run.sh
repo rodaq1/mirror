7dc0eef73e0ef165a871d94adc2df183ab778fab
rodaq1 <167247609+rodaq1@users.noreply.github.com>
2025-05-06 12:25:21 +0200

    XD

--- run.sh
+++ run.sh
@@ -3,5 +3,16 @@ cd "$(dirname "$0")"
 python3 -m venv venv
 source venv/bin/activate
 pip install --upgrade pip
-pip install -r requirements.txt
+pip install SpeechRecognition
+pip install googletrans
+pip install google-generativeai
+pip install azure-cognitiveservices-speech
+pip install certifi
+pip install sentence-transformers
+pip install spotipy
+pip install urllib3
+pip install requests
+pip install protobuf
 python3 main.py
+
+10.90.10.61
